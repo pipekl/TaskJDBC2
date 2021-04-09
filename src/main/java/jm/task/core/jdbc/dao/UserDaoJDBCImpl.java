8e838e76de7e65823a6b8338a98c84caf7f6585e
pipekl <79262323423@yandex.ru>
2021-04-09 17:53:29 +0300

    change connect

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,9 +1,11 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
-import jm.task.core.jdbc.service.UserServiceImpl;
 import jm.task.core.jdbc.util.Util;
-
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
@@ -11,32 +13,69 @@ public class UserDaoJDBCImpl implements UserDao {
 
     public void createUsersTable() {
         String sql = "CREATE TABLE IF NOT EXISTS pensionniiFond (id bigint primary key auto_increment, name varchar(100), lastName varchar(100), age tinyint);";
-        util.connect(sql);
+        try (Statement statement = Util.connect().createStatement()) {
+            statement.execute(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public void dropUsersTable() {
         String sql = "DROP TABLE IF EXISTS pensionniiFond";
-        util.connect(sql);
+        try (Statement statement = Util.connect().createStatement()) {
+            statement.execute(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public void saveUser(String name, String lastName, byte age) {
         String sql = "INSERT pensionniiFond (name, lastName, age) VALUES ('"
                 + name + "', '" + lastName + "', " + age + " );";
-        util.connect(sql);
+        try (Statement statement = Util.connect().createStatement()) {
+            statement.execute(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         System.out.println("User с именем – " + name + " добавлен в базу данных pensionniiFond");
     }
 
     public void removeUserById(long id) {
         String sql = "DELETE FROM pensionniiFond WHERE id = " + id;
-        util.connect(sql);
+        try (Statement statement = Util.connect().createStatement()) {
+            statement.execute(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public List<User> getAllUsers() {
-        return util.connectShow();
+        String sql = "SELECT * FROM pensionniiFond";
+        List<User> users = new ArrayList<>();
+
+        try (Statement statement = Util.connect().createStatement()) {
+            ResultSet resultSet = statement.executeQuery(sql);
+
+            while (resultSet.next()) {
+                User user = new User();
+                user.setId(resultSet.getLong("id"));
+                user.setName(resultSet.getString("name"));
+                user.setLastName(resultSet.getString("lastName"));
+                user.setAge(resultSet.getByte("age"));
+                users.add(user);
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return users;
     }
 
     public void cleanUsersTable() {
         String sql = "TRUNCATE TABLE pensionniiFond";
-        util.connect(sql);
+        try (Statement statement = Util.connect().createStatement()) {
+            statement.execute(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,56 +1,23 @@
 package jm.task.core.jdbc.util;
+
 import com.mysql.cj.jdbc.NonRegisteringDriver;
-import jm.task.core.jdbc.model.User;
 import java.sql.*;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Util {
     private static final String URL = "jdbc:mysql://localhost:3306/users";
     private static final String USERNAME = "root";
     private static final String PASSWORD = "root";
 
-    public void connect (String operation) {
+    public static Connection connect() {
+        Connection connection = null;
+
         try {
             Driver driver = new NonRegisteringDriver();
             DriverManager.registerDriver(driver);
-        } catch (
-                SQLException throwables) {
-            throwables.printStackTrace();
-        }
-
-        try (Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD); Statement statement = connection.createStatement()) {
-            statement.executeUpdate(operation);
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
         } catch (SQLException throwables) {
             throwables.printStackTrace();
         }
-    }
-
-    public List<User> connectShow () {
-        String sql = "SELECT * FROM pensionniiFond";
-        List <User> users = new ArrayList<>();
-        try {
-            Driver driver = new NonRegisteringDriver();
-            DriverManager.registerDriver(driver);
-        } catch (
-                SQLException throwables) {
-            throwables.printStackTrace();
-        }
-
-        try (Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD); Statement statement = connection.createStatement()) {
-            ResultSet resultSet = statement.executeQuery(sql);
-
-            while (resultSet.next()) {
-                User user = new User();
-                user.setId(resultSet.getLong("id"));
-                user.setName(resultSet.getString("name"));
-                user.setLastName(resultSet.getString("lastName"));
-                user.setAge(resultSet.getByte("age"));
-                users.add(user);
-            }
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        }
-        return users;
+        return connection;
     }
 }
